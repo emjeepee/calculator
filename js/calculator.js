081f8a3a0e0eafdc11cabfed981ed61e853206aa
Mukund Pandit <mukund.pandit@virginmedia.com>
2021-08-22 19:50:36 +0100

    first commit ever of this project

---
+++ js/calculator.js
@@ -0,0 +1,13 @@
+
+// INITIALISATION:
+const load = () => {
+
+                    }
+window.onload = load;
+// end INITIALISATION:
+
+
+
+// Some constants to represent elements:
+// First the container with purple linear grad border:
+// const container1 = document.querySelector('#container1');
